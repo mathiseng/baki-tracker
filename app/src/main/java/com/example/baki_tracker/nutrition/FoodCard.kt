32101e33a09791eb4372ab5ed5dd68e442c542a9
gbensch00 <gbensch00@gmail.com>
2024-12-19 10:36:17 +0100

    add foodSummaryCard

---
+++ app/src/main/java/com/example/baki_tracker/nutrition/FoodCard.kt
@@ -0,0 +1,15 @@
+package com.example.baki_tracker.nutrition
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FoodCard(
+    name: String,
+    calories: Int,
+    protein: Int,
+    carbs: Int,
+    fats: Int,
+
+){
+
+}
